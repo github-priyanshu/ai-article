aa8c762f01915e5faa3766e269cb9d7efaa6fca6
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-10-31 12:26:43 +0530

    Add files via upload

--- page/seo.js
+++ page/seo.js
@@ -33,3 +33,45 @@ function makeTags(){
 
 }
 makeTags();
+
+/*REDIRECT UI MAKER*/
+var downloadData={src: redirect.lnk};
+
+if(redirect){
+  heads.style.display='';
+  var name='To Continue :';
+  if(redirect.mid){
+    downloadData=movies[redirect.mid];
+    downloadData.src="https://ai-player.netlify.app?sh=8&mid="+redirect.mid;
+    name='Download "'+downloadData.name+'"';
+  }
+  heads.innerHTML=`<h1 class="name">${name}</h1>
+      <h2 class="release texCen">Scroll Down</h2>`;
+
+  makeLinkUi();
+}
+
+function makeLinkUi(){
+  var html=`
+  <div class='flex c w100p'>
+    <h2 id="gdTxt">Please wait. Creating link...</h2>
+    <button id="redBtn" style=
+    'padding: 10px;background: #008400;border-radius: 5px;border: 1px solid #0a0;color: #fff;font-size: 1.1em;'
+    onclick="window.open('${downloadData.src}')"
+    disabled>Please Wait</button>
+  </div>`;
+  op(".eng").insertAdjacentHTML("afterend",html);
+
+  setTimeout(()=>{
+    var rb=op("#redBtn");
+    rb.disabled=false;
+    rb.innerHTML="Continue...";
+
+    op('#gdTxt').innerHTML="Click to Continue now..."
+  },8000)
+}
+
+/*
+?redirect="{\"lnk\":\"https://sldkjf\"}"
+?redirect="{\"mid\":\"3\"}"
+*/
